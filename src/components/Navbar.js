c82f02749500a7a1987385f7c95707023fbd9f1d
3nsar <84131267+3nsar@users.noreply.github.com>
2022-11-17 11:44:43 +0100

    commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar-container'>
+       <h1>QUIZ</h1>
+       <img src="" alt="pic" />
+    </div>
+  )
+}
+
+export default Navbar
