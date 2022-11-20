8c3000d1631ed6955f131e242e192323ed04d2e5
3nsar <84131267+3nsar@users.noreply.github.com>
2022-11-20 21:57:46 +0100

    commit

--- src/questions-answers/Question.js
+++ src/questions-answers/Question.js
@@ -6,7 +6,7 @@ const Question = ({question}) => {
   return (
     <div>{question.map((item)=>(
         <div key={item.id}>
-          <h2>{item.question}</h2>
+          <h1>{item.question}</h1>
           <h3>{item.correctAnswer}</h3>
           <h3>{item.incorrectAnswers}</h3>
         </div>
