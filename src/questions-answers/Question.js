0c381b56841c991c9c0db5781cee9e8e9b27006d
3nsar <84131267+3nsar@users.noreply.github.com>
2022-11-22 12:06:05 +0100

    update

--- src/questions-answers/Question.js
+++ src/questions-answers/Question.js
@@ -5,31 +5,37 @@ import axios from 'axios'
 
 
 const Question = () => {
-
-  const [position, setPosition] = useState(0)
   const [score, setScore] = useState(0)
   const [isCorrect, setIsCorrect] = useState(false)
   const [isFalse, setIsFalse] = useState(false)
   
-  const num = 1;
-
-  const url = `https://the-trivia-api.com/api/questions?limit=${num}`
-  //const url = "https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple"
-  const [question, setQuestion] = useState([]);
+  const [questions, setQuestions] = useState([]);
   const [loading, setLoading] = useState(false)
-  const [nextQuestion, setNextQuestion] = useState(0)
+  const [nextQuestion, setNextQuestion] = useState(1)
+
+
+//url bei questionm
+
+  const url = `https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple`
+
+
 
   useEffect(()=>{
     const fetchPosts = async () =>{
      setLoading(true)
       const res = await axios.get(url);
       console.log(res)
-      setQuestion(res.data)
+      setQuestions(res.data)
       console.log(res.data)
-      }
+    }
   fetchPosts()
   setLoading(false)
   },[])
+
+  
+  const showNextQuestion = () =>{
+    setNextQuestion(prev => prev +1)
+  }
 
   const handleScore = () =>{
     setIsCorrect(prev => !prev)
@@ -39,20 +45,18 @@ const Question = () => {
   const handleFalse = () =>{
     setIsFalse(prev => !prev)
   }
-  
+
   return (
     <div className='question-container'>
-      {question.map((item)=>(
-        <div className='question-content' key={item.id}>
-          <h2>{item.question}</h2>
-          <h3 onClick={handleScore} style={{backgroundColor: isCorrect ? "green" : ""}}>{item.correctAnswer}</h3>
-          <h3 onClick={handleFalse} style={{backgroundColor: isFalse ? "red" : ""} }>{item.incorrectAnswers[position]}</h3>
-          <h3 onClick={handleFalse} style={{backgroundColor: isFalse ? "red" : ""}}>{item.incorrectAnswers[position + 1]}</h3>
-          <h3 onClick={handleFalse} style={{backgroundColor: isFalse ? "red" : ""}}>{item.incorrectAnswers[position + 2]}</h3>
+        <div className='question-content'>
+          <h2>{questions[0].question}</h2>
+          <h3 onClick={handleScore} style={{backgroundColor: isCorrect ? "green" : ""}}>{questions[0].correct_answer}</h3>
+          <h3 onClick={handleFalse} style={{backgroundColor: isFalse ? "red" : ""} }>{questions.incorrect_answers[0]}</h3>
+          <h3 onClick={handleFalse} style={{backgroundColor: isFalse ? "red" : ""}}>{questions.incorrect_answers[1]}</h3>
+          <h3 onClick={handleFalse} style={{backgroundColor: isFalse ? "red" : ""}}>{questions.incorrect_answers[2]}</h3>
+          <button onClick={showNextQuestion}>next</button>
           <h4>You score {score} / 5</h4>
-
         </div>
-    ))}
     </div>
   )
 }
