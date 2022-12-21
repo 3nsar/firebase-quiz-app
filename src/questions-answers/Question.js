8195864237e6911500ab0a920040269ea422a637
3nsar <84131267+3nsar@users.noreply.github.com>
2022-12-21 12:48:31 +0100

    added colors to answers

--- src/questions-answers/Question.js
+++ src/questions-answers/Question.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 
 
-const Question = ({handleAnswer, showAnswer,handleNextQuestion, data: {question, correctAnswer, incorrectAnswers}}) => {
+const Question = ({handleAnswer, handleNextQuestion,showAnswer, data: {question, correctAnswer, incorrectAnswers}}) => {
 
   const swappedAnswers = [correctAnswer, ...incorrectAnswers].sort(() => Math.random() -0.5);
 
@@ -11,12 +11,12 @@ const Question = ({handleAnswer, showAnswer,handleNextQuestion, data: {question,
         <div className='question-content'>
           <h4>Time left: 00:00</h4>
           <h2>{question}</h2>
-          {swappedAnswers.map((answer) => (
+          {swappedAnswers.map((answer) => {
+            const bgColor = showAnswer ? answer === correctAnswer ? 'correct-bg' : 'incorrect-bg' : 'white-bg'
+          return(
+          <h3 className={`${bgColor}`} onClick={() => handleAnswer(answer)}>{answer}</h3>
 
-            
-          <h3 className={`${correctAnswer === answer ? 'correct-bg' : 'white-bg'}`} onClick={() => handleAnswer(answer)}>{answer}</h3>
-
-          ))}
+          )})}
 
           {showAnswer && (
              <button className='next-btn' onClick={handleNextQuestion}>Next Question</button>
