5ad94191b56010fb4146a39b96f743fc002af969
3nsar <84131267+3nsar@users.noreply.github.com>
2022-12-23 10:27:07 +0100

    added time limit

--- src/questions-answers/Question.js
+++ src/questions-answers/Question.js
@@ -3,24 +3,24 @@ import { FcAlarmClock } from 'react-icons/fc'
 
 
 
-const Question = ({handleAnswer, handleNextQuestion,showAnswer, data: {question, correctAnswer, answers}}) => {
-  const [counter, setCounter] = useState(10)
+const Question = ({handleAnswer, handleNextQuestion,showAnswer,score, data: {question, correctAnswer, answers}}) => {
+  const [counter, setCounter] = useState(60)
 
   React.useEffect(() => {
     counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
   }, [counter]);
 
 
-  return (
+  return counter != 0 ?(
     <div className='question-container'>
       <h2><FcAlarmClock />{counter}</h2>
         <div className='question-content'>
           <h2>{question}</h2>
+
           {answers.map((answer) => {
             const bgColor = showAnswer ? answer === correctAnswer ? 'correct-bg' : 'incorrect-bg' : 'white-bg'
           return(
           <h3 className={`${bgColor}`} onClick={() => handleAnswer(answer)}>{answer}</h3>
-
           )})}
 
           {showAnswer && (
@@ -28,6 +28,8 @@ const Question = ({handleAnswer, handleNextQuestion,showAnswer, data: {question,
           )}
         </div>
     </div>
+  ):(
+    <h1>You didn't make it to end! Try again!</h1>
   )
 }
 
