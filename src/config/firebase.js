4dfc09171b15fbc2d280fec79a6c3e6d81f10e9c
3nsar <84131267+3nsar@users.noreply.github.com>
2023-06-03 22:22:26 +0200

    firebase hosting

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -13,25 +13,23 @@ import {getStorage} from "firebase/storage"
 
 export const firebaseConfig = {
 
-  apiKey: process.env.REACT_APP_apiKey,
+  apiKey: process.env.REACT_APP_API_KEY,
 
-  authDomain:  process.env.REACT_APP_authDomain ,
+  authDomain:  process.env.REACT_APP_AUTH_DOMAIN,
 
-  projectId:  process.env.REACT_APP_projectId,
+  projectId:  process.env.REACT_APP_PROJECT_ID,
 
-  storageBucket:  process.env.REACT_APP_storageBucke,
+  storageBucket:  process.env.REACT_APP_STORAGE_BUCKET,
 
-  messagingSenderId: process.env.REACT_APP_messagingSenderId ,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID ,
 
-  appId:  process.env.REACT_APP_appId
+  appId:  process.env.REACT_APP_APP_ID
 
 };
 
 
 // Initialize Firebase
 
-console.log("api key:", process.env.REACT_APP_API_KEY)
-console.log("sii:", process.env.REACT_APP_SII)
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
